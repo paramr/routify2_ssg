690e4cd8accd413449ee2daa00330e848319a7cd
Param Reddy <param@spinorx.com>
2021-12-08 20:43:00 -0800

    Sample for routify2 with vite

---
+++ src/pages/index.svelte
@@ -0,0 +1,19 @@
+<!-- routify:option name="example-app" -->
+
+<div style="width: 100%; text-align: center; margin-top: 4rem;">
+  <div>
+    <b>Guide:</b>
+    <br />
+    <a href="https://routify.dev">https://routify.dev</a>
+  </div>
+
+  <br />
+  <div>
+    <b>This template:</b>
+    <br />
+    <a href="https://github.com/roxiness/routify-starter">
+      https://github.com/roxiness/routify-starter
+    </a>
+  </div>
+
+</div>
