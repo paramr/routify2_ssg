adf3034d2ad4db33509b689e7fff29065cfc912b
Param Reddy <param@spinorx.com>
2021-12-08 21:52:31 -0800

    Add vite-main-js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+const viteMainJs = require('vite-main-js')
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    server: {
-        port: 5000,
-        host: '0.0.0.0',
-    },
-    
-    plugins: [svelte()],
+  server: {
+      port: 5000,
+      host: '0.0.0.0',
+  },
+
+  plugins: [
+    viteMainJs(),
+    svelte({
+      preprocess: [],
+      emitCss: true,
+    }),
+  ],
 });
