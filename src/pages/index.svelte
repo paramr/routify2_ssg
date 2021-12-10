83d737a62f173958d2716fb6f0455368f9a4cccf
Param Reddy <param@spinorx.com>
2021-12-09 17:24:50 -0800

    Enable smui

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,11 +1,58 @@
 <!-- routify:option name="example-app" -->
 <script lang="ts">
-  import NavLinks from "./_components/NavLinks.svelte";
+  import { url, isActive } from "@roxi/routify";
+  const _links = [
+    ["./index", "Home"],
+    ["./dynamic/test", "Api"],
+    ["./reset", "Reset"],
+    ["./layouts", "Layouts"],
+  ];
+
+  import TopAppBar, {
+    Row,
+    Section,
+    Title,
+    AutoAdjust,
+    TopAppBarComponentDev,
+  } from '@smui/top-app-bar';
+  import IconButton from '@smui/icon-button';
+  import Button, { Label } from '@smui/button';
+
+  let topAppBar: TopAppBarComponentDev;
+
   export let hello: string = "Hello world"
 </script>
 
-<NavLinks />
+<TopAppBar bind:this={topAppBar} variant="standard" dense>
+  <Row>
+    <Section>
+      <IconButton class="material-icons">menu</IconButton>
+      <Title>Dense</Title>
+    </Section>
+    <Section align="end" toolbar>
+      <IconButton class="material-icons" aria-label="Download"
+        >file_download</IconButton
+      >
+      <IconButton class="material-icons" aria-label="Print this page"
+        >print</IconButton
+      >
+      <IconButton class="material-icons" aria-label="Bookmark this page"
+        >bookmark</IconButton
+      >
+    </Section>
+  </Row>
+</TopAppBar>
+
+
+<AutoAdjust {topAppBar}>
 <div style="width: 100%; text-align: center; margin-top: 4rem;">
+  <nav>
+    {#each _links as [path, name]}
+      <a class="link" class:active={$isActive(path)} href={$url(path)}>
+        {name}
+      </a>
+    {/each}
+  </nav>
   <div>
     <b>Guide:</b>
     <br />
@@ -25,3 +72,4 @@
   <div>From code: {hello}</div>
 
 </div>
+</AutoAdjust>
